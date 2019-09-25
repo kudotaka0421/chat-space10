70e6c25faf2c3967fea3a9eed6c82268d3b4cc73
kudotaka0421 <zmk1n421@gmail.com>
2019-09-25 11:23:08 +0900

    コミット

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, unless: :image?
+                                      #画像がある場合は適用外ですよという意味・
+  mount_uploader :image, ImageUploader
 end
